302d1b8f6d02223094b5773f77dd7107978ce212
alokkksharmaa <aloksharma1097@gmail.com>
2026-08-29 10:56:32 +0530

    files handling and unit 3

--- unit3/niofile.java
+++ unit3/niofile.java
@@ -2,16 +2,64 @@ import java.nio.file.*;
 // path = store a path
 // paths = generate the file
 // files = create the file
-
 // WAP to create a file
 
 class niofile {
   public static void main(String[] args) throws Exception {  
-    Path folder = Paths.get("C:\\Users\\aloks\\Java\\Unit3\\tests");
-    Files.createDirectories(folder);
-    System.out.println("Directory created successfully");
-    Path f = Paths.get("C:\\Users\\aloks\\Java\\Unit3\\lpu.txt");
-    Files.createFile(f);
-    System.out.println("File created");
+    // Path folder = Paths.get("C:\\Users\\aloks\\Java\\Unit3\\tests");
+    // // command to create a folder or directories.
+    // Files.createDirectories(folder);
+    // System.out.println("Directory created successfully");
+    // Path f = Paths.get("C:\\Users\\aloks\\Java\\Unit3\\lpu.txt");
+    // if(!Files.exists(f)){
+    //   Files.createFile(f);
+    //   System.out.println("hello");
+    // }
+    // // command to create a file 
+    // System.out.println("File created");
+
+    // // important path methods:
+    // System.out.println(f.getFileName()); // lpu.txt
+
+    // System.out.println(f.getParent()); // C:\Users\aloks\Java\Unit3
+
+    // System.out.println(f.getRoot()); // C:\
+
+
+    //  how create a relative path
+    Path p1 = Paths.get("lpunew.txt");
+    // Files.createFile(p1);
+    System.out.println(p1);
+
+
+    // how to convert  a relative path to a absolute path
+    p1 = p1.toAbsolutePath();
+    // System.out.println(p1);
+
+    // wap to check wether it's a file or a directory
+    // 2 methods (i)Files.isregularFile (ii)  Files.isDirectory
+    // System.out.println(Files.isRegularFile(folder));
+    // System.out.println(Files.isDirectory(f)); 
+    
+    // wap to create a directory in current pwd
+    Path p2 = Paths.get("MynewFolder");
+    // Files.createDirectories(p2);
+
+    // create multiple directories
+    Path p3 = Paths.get("MyNewFolder\\Cse\\Lpu");
+    Files.createDirectories(p3);
+
+    // how to delete a file
+    Files.delete(p3);
+
+    // wap to delete a file if exists
+    Files.deleteIfExists(p3);
+
   } 
 }
+
+
+// types of file
+// (i)abs == pwd  ex :- "C:\\Users\\aloks\\Java\\Unit3\\tests"
+// (ii)relative   ex :- paths.get("lpu.txt"); or paths.get("fs", lpu.txt);
+// 
